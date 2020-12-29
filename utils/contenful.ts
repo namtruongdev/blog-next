d5d2ffa7b57de1cb0fc286863d16426e5257d708
NAM TRUONG DUONG <namtruong@namtruong-pc>
2020-12-29 09:41:22 +0700

    update

--- utils/contenful.ts
+++ utils/contenful.ts
@@ -1,6 +1,22 @@
 import * as contentful from 'contentful';
 
-export const client = contentful.createClient({
+const client = contentful.createClient({
   space: process.env.NEXT_CONTENTFUL_SPACE_ID || '',
   accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN || '',
 });
+
+const CONTENT_TYPE_ARTICLE = 'article';
+const CONTENT_TYPE_CATEGORY = 'category';
+
+export const getPostBySlug = async (slug: any) => {
+  const data = await client.getEntries({
+    content_type: CONTENT_TYPE_ARTICLE,
+    'fields.slug': slug,
+  });
+  const post = data.items.map(({ sys, fields }: { sys: any; fields: any }) => ({
+    id: sys.id,
+    title: fields.title,
+    body: fields.body,
+  }));
+  return post;
+};
